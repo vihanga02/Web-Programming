98408111393dafe8ffdffb1a2fdd71c932e511f1
Vihanga Muthumala <149398329+vihanga02@users.noreply.github.com>
2024-06-06 21:42:17 +0530

    chore: Update Home component with useEffect and loading state

--- User_Interfaces/Net Ninja/dojo-blog/src/Home.js
+++ User_Interfaces/Net Ninja/dojo-blog/src/Home.js
@@ -1,23 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
+  const [blogs, setBlogs] = useState(null);
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.fileter(blog =>  id !== blog.id);
-        setBlogs(newBlogs);
-    }
+  const [name, setName] = useState('mario');
+  const [isPending, setIsPending] = useState(true);
 
-    return ( 
-        <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
-        </div>
-    );
+//   const handleDelete = (id) => {
+//     const newBlogs = blogs.filter(blog => blog.id !== id);
+//     setBlogs(newBlogs);
+//   }
+
+  useEffect(() => { 
+    fetch('http://localhost:8000/blogs')
+        .then(response => {
+            return response.json()
+        })
+        .then((data) => {
+            console.log(data)
+            setBlogs(data)
+            setIsPending(false);
+        })
+  }, [])
+
+  return (
+    <div className="home">
+        {isPending && <div>Loading...</div>}
+        {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
+    </div>
+  );
 }
  
 export default Home;

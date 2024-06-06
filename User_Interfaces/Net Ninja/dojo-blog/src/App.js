d17e317988f769bd39c0bddaa5ac87d133fe998d
Vihanga Muthumala <149398329+vihanga02@users.noreply.github.com>
2024-06-07 00:22:50 +0530

    feat: Add react-router-dom for navigation in Navbar component

--- User_Interfaces/Net Ninja/dojo-blog/src/App.js
+++ User_Interfaces/Net Ninja/dojo-blog/src/App.js
@@ -1,17 +1,25 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-
   return (
-    <div className="App">
-       <Navbar />
-       <div className="content">
-          <Home />
-       </div>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- User_Interfaces/Net Ninja/dojo-blog/src/Home.js
+++ User_Interfaces/Net Ninja/dojo-blog/src/Home.js
@@ -1,31 +1,17 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-
-  const [name, setName] = useState('mario');
-  const [isPending, setIsPending] = useState(true);
+    const {data : blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
 
 //   const handleDelete = (id) => {
 //     const newBlogs = blogs.filter(blog => blog.id !== id);
 //     setBlogs(newBlogs);
 //   }
 
-  useEffect(() => { 
-    fetch('http://localhost:8000/blogs')
-        .then(response => {
-            return response.json()
-        })
-        .then((data) => {
-            console.log(data)
-            setBlogs(data)
-            setIsPending(false);
-        })
-  }, [])
-
   return (
     <div className="home">
+        { error && <div>{error}</div>}
         {isPending && <div>Loading...</div>}
         {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
     </div>

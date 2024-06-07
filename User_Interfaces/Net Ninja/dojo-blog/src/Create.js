605f81ee50927695f880c288ec16d55e299f0660
Vihanga Muthumala <149398329+vihanga02@users.noreply.github.com>
2024-06-07 11:57:33 +0530

    chore: Add NotFound component and route for handling 404 errors

--- User_Interfaces/Net Ninja/dojo-blog/src/Create.js
+++ User_Interfaces/Net Ninja/dojo-blog/src/Create.js
@@ -1,23 +1,26 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const Create = () => {
-
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
+  const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
 
   const handleSubmit = (e) => {
-    e.prevebtDefault();
-    const blog = { title, body, author};
+    e.preventDefault();
+    const blog = { title, body, author };
 
     fetch('http://localhost:8000/blogs/', {
       method: 'POST',
-      headers: { "Content-Type": "application/json"},
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(blog)
-    })
-    .then(() => {
+    }).then(() => {
       console.log('new blog added');
-    })
+      setIsPending(false);
+      history.push('/');
+    }) 
   }
 
   return (
@@ -25,21 +28,31 @@ const Create = () => {
       <h2>Add a New Blog</h2>
       <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
-        <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)}/>
+        <input 
+          type="text" 
+          required 
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
         <label>Blog body:</label>
-        <textarea required value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
         <label>Blog author:</label>
-        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+        <select
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add Blog</button>
-        <p>{ title }</p>
-        <p>{ body }</p>
-        <p>{ author }</p>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding Blog...</button>}
       </form>
     </div>
   );
 }
-   
+ 
 export default Create;

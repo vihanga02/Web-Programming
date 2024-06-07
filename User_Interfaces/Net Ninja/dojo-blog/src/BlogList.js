385284add0f355ea16b2117d1194d6d5b9b5bd53
Vihanga Muthumala <149398329+vihanga02@users.noreply.github.com>
2024-06-07 09:15:57 +0530

    feat: Add BlogDetails component and route for individual blog details

--- User_Interfaces/Net Ninja/dojo-blog/src/BlogList.js
+++ User_Interfaces/Net Ninja/dojo-blog/src/BlogList.js
@@ -1,13 +1,17 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
 const BlogList = ({blogs, title, handleDelete}) => {
     
+
     return ( 
         <div className="blog-list">
             <h2>{title}</h2>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                    <button onClick={() => handleDelete(blog.id)}>delete Blog</button>
+                    <Link to={`/blogs/${blog.id}`}>
+                        <h2>{blog.title}</h2>
+                        <p>Written by {blog.author}</p>
+                    </Link>  
                 </div>
             ))}
         </div>
